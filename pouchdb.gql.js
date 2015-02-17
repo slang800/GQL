99feb7297e9f1bd1aff6c1cd37e4e3adceff8118
Sean Lang <slang800@gmail.com>
2015-02-17 12:14:57 -0500

    move errors into separate file

--- pouchdb.gql.js
+++ pouchdb.gql.js
@@ -1,5 +1,8 @@
 "use strict";
 var pouchCollate = require('pouchdb-collate');
+var GQL = {
+  Errors: require('./lib/errors')
+};
 
 exports.gql = function (fun, opts, callback) {
   if (typeof opts === 'function') {
@@ -968,43 +971,6 @@ function viewQuery(db, query, options) {
     }
   });
 }
-var GQL = {};
-GQL.Errors = {
-  UNRECOGNIZED_QUERY: {
-    status: 400,
-    error: 'not_recognized',
-    reason: 'Format of input query is not valid'
-  },
-  LEXER_ERROR: function (message, index) {
-    return {
-      status: 400,
-      error: "tokenizing_error",
-      reason: message + " on character " + index + "." || "Generic tokenizing error"
-    };
-  },
-  PARSING_ERROR: function (message) {
-    return {
-      status: 400,
-      error: "parsing_error",
-      reason: message || "Generic parsing error"
-    };
-  },
-  SELECT_ERROR: function (message) {
-    return {
-      status: 400,
-      error: "select_error",
-      reason: message || "Generic select error"
-    };
-  },
-  PIVOT_ERROR: function (message) {
-    return {
-      status: 400,
-      error: "pivot_error",
-      reason: message || "Generic pivot error"
-    };
-  }
-};
-
 
 if (typeof window !== 'undefined' && window.PouchDB) {
   window.PouchDB.plugin(exports);
